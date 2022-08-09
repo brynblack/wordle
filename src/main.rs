ad4d1f08dba82bbc4d57684dd9ce8e400b1180bf
Brynley Llewellyn-Roux <49110391+brynblack@users.noreply.github.com>
2022-08-09 11:15:16 +1000

    Added max guesses

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 const WORD: &str = "girl";
+const MAX_GUESSES: u8 = 6;
 
 fn main() {
+    let mut tries = 0;
     std::io::stdin().lines().for_each(|guess| {
         let guess = guess.unwrap();
 
+        // Check if guess length is not the same as word length
         if guess.len() != WORD.len() {
             println!("Word length is {} letters!", WORD.len());
             return;
         }
 
+        // Check if tries is larger than max guesses
+        if tries >= MAX_GUESSES {
+            println!("You Lost! The word was {}", WORD);
+            std::process::exit(0);
+        }
+
+        // Filter the out the correct letters from the guess
         let correct_letters: String = guess
             .chars()
             .filter(|&letter| WORD.contains(letter))
@@ -18,7 +28,9 @@ fn main() {
             println!("You Win!");
             std::process::exit(0);
         } else {
+            tries += 1;
             println!("Correct letters: {}", correct_letters);
+            println!("You have {} guesses remaining", MAX_GUESSES - tries);
         }
     });
 }
